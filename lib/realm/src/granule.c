aecad126cf873639b66bb99d84267bc08f4dce2b
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-01-16 13:05:59 +0000

    fix(granules): granule_addr() did not detect a misaligned address
    granule_addr() API on the granule module of the realm library should
    cause an assertion failure when the struct granule address pointer
    received as parameter is not properly aligned.
    
    This patch also adds the proper unittest to verify the condition.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I49df885af5dc346c33c04bc88fcdc1db36f75fcd

--- lib/realm/src/granule.c
+++ lib/realm/src/granule.c
@@ -29,6 +29,7 @@ unsigned long granule_addr(const struct granule *g)
 	unsigned long idx;
 
 	assert(g != NULL);
+	assert(ALIGNED((void *)g, sizeof(struct granule)));
 	assert(g >= &granules[0]);
 
 	idx = g - &granules[0];
