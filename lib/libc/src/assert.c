e9e3471dd0c5101cb9e4ebf2fa51f8f24a72c64d
Soby Mathew <soby.mathew@arm.com>
2023-04-20 15:28:59 +0000

    fix(lib/libc): remove incorrect __assert()
    Depending on the version libc associated with the compiler,
    the signature of __assert() is different and RMM
    compilation fails. The patch removes this as it was not
    used in the systems tested. Also add an implementation
    for __assert_fail() which is needed for certain versions
    of libc.
    
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>
    Change-Id: Idd9a47b568e4b6e3a4a6a00618d49e140f75c60a

--- lib/libc/src/assert.c
+++ lib/libc/src/assert.c
@@ -7,14 +7,6 @@
 #include <assert.h>
 #include <debug.h>
 
-void __assert(const char *file, int line, const char *expression)
-{
-	ERROR("Assertion \"%s\" failed %s:%d\n", expression, file, line);
-	while (true) {
-		wfe();
-	}
-}
-
 void __assert_func(const char *file, int line, const char *func, const char *expression)
 {
 	ERROR("Assertion \"%s\" failed %s:%d, %s\n", expression, file, line, func);
@@ -23,4 +15,12 @@ void __assert_func(const char *file, int line, const char *func, const char *exp
 	}
 }
 
-
+void __assert_fail(const char *expression, const char *file, unsigned int line, const char *func)
+{
+	/* Ignore func as it can be NULL */
+	(void)func;
+	ERROR("Assertion \"%s\" failed %s:%d\n", expression, file, line);
+	while (true) {
+		wfe();
+	}
+}
