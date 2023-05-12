02443e95033d9e6a12772b9aa6c7e17929662aa0
Soby Mathew <soby.mathew@arm.com>
2023-05-12 10:49:27 +0100

    build: Use backup variable when modifying C/CXX flag
    Some components like CppUTest and MbedTLS need different
    CXX and C Flags and this patch ensures that when these flags are
    modified, the global settings are not changed. This is done
    by using a backup variable when the flags are modified and
    restoring them afterwards.
    
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>
    Change-Id: Ic7f7eec104e2d43ee1bd2ea1f56c08758f93b494

--- cmake/BuildCppUTest.cmake
+++ cmake/BuildCppUTest.cmake
@@ -5,9 +5,10 @@
 
 #
 # Remove Werror from CXXFLAGS else CppUTest compiler checks will fail.
-# This will affect CMAKE_CXX_FLAG in the current scope and parent scope
-# is unaffected.
+# Ensure that CMAKE_CXX_FLAG is changed only for the current scope and
+# parent scope is unaffected.
 #
+set(BACKUP_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 string(REPLACE "-Werror" " " CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 
 # Additional CXXFLAGS to get CppUTest to compile.
@@ -15,3 +16,5 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-c++98-compat-pedantic ")
 
 add_subdirectory("ext/cpputest")
 
+# Restore the original CXX flags.
+set(CMAKE_CXX_FLAGS "${BACKUP_CXX_FLAGS}")
