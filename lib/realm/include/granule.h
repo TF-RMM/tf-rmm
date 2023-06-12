40b3bf0c7f9d77de8a19aabd7177503c334f5829
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2023-06-12 12:19:55 +0100

    fix(realm): initialize rec aux data during rec create
    After a REC create, its likely a REC enter call will be made. This
    change initializes the REC aux granule pages that are used for
    attestation heap, PMU and SIMD during REC create. This avoids every REC
    enter call to initialize these data regions.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: Ib38cfebcd22f082805fb8aaee6c3a597f4092230

--- lib/realm/include/granule.h
+++ lib/realm/include/granule.h
@@ -147,6 +147,9 @@ void granule_memzero(struct granule *g, enum buffer_slot slot);
 
 void granule_memzero_mapped(void *buf);
 
+void *aux_granules_map(struct granule *rec_aux_pages[], unsigned int num_aux);
+void aux_granules_unmap(void *rec_aux, unsigned int num_aux);
+
 /* Must be called with g->lock held */
 static inline void __granule_get(struct granule *g)
 {
