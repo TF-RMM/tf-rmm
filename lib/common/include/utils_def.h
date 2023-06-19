ac0cfbb0328067f2849f174917e15e02ed98827d
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-06-19 16:29:52 +0200

    refactor(lib): define GRANULE_SHIFT as parameter
    GRANULE_SHIFT is defined as build time parameter instead of
    GRANULE_SIZE. This makes it possible to derive GRANULE_SIZE and
    GRANULE_MASK from a single value.
    
    Change-Id: I8e6a7aa148aade9921b062702b24111d96107dbf
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- lib/common/include/utils_def.h
+++ lib/common/include/utils_def.h
@@ -135,8 +135,9 @@
 			(((uintptr_t)_addr >= (uintptr_t)&_array[0]) && \
 			 ((((uintptr_t)_addr - (uintptr_t)&_array[0]) % \
 						sizeof(_array[0])) == UL(0)))
-#define GRANULE_SHIFT	(U(12))
-#define GRANULE_MASK	(~0xfffUL)
+
+#define GRANULE_SIZE	(UL(1) << GRANULE_SHIFT)
+#define GRANULE_MASK	(~(GRANULE_SIZE - 1U))
 
 #define HAS_MPAM 0
 
