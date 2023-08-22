4bbb0e0ed6d38de8554c4be07fce3605b3dbab5e
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-08-22 14:15:21 +0100

    fix(misra): fix MISRA-C:2012 8.7 in lib/attestation/src
    This patch fixes MISRA-C:2012 Rule 8.7 violation by
    making some objects and APIs in lib/attestation static.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: Ief00c12be5a924bcd550d5c2ee239cf5ba72f634

--- lib/attestation/src/attestation_priv.h
+++ lib/attestation/src/attestation_priv.h
@@ -48,19 +48,6 @@ int attest_get_realm_signing_key(psa_key_handle_t *key_handle);
 int attest_init_realm_attestation_key(void);
 
 /*
- * Get the hash of the realm attestation public key. The public key hash is the
- * challenge value in the platform attestation token.
- *
- * Arguments:
- * public_key_hash - Get the buffer address and size which holds
- *                   the hash of the realm attestation public key.
- *
- * Returns 0 on success, negative error code on error.
- *
- */
-int attest_get_realm_public_key_hash(struct q_useful_buf_c *public_key_hash);
-
-/*
  * Get the realm attestation public key hash. The public key hash is the
  * challenge value in the platform attestation token.
  *
