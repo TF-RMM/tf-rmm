ebfab6adfee6b1372ffe0194cbd1a7468b4936d1
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2023-08-24 14:44:23 +0100

    feat(runtime): handle SVE hint bit passed in RMI SMCs
    Extract the SVE hint bit passed in RMI SMC function id and update it
    to the NS SIMD context.
    
    This SVE hint bit will be later used while saving or restoring NS SIMD
    context.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: If4dd5c3c0c8108c48e2bea6ed241e0c0cd51883b

--- lib/smc/include/smc.h
+++ lib/smc/include/smc.h
@@ -30,8 +30,13 @@
 #define SMC_MBZ_MASK		U(0xFF)
 #define SMC_MBZ_ZERO		U(0x0)
 
-/* SVE Hint bit (SMCCCv1.3), denoting the absence of SVE specific live state */
-#define SMC_SVE_HINT		(U(1) << 16)
+/*
+ * SVE Hint bit (SMCCCv1.3), denoting the absence of SVE specific live state.
+ *
+ * MISRA treats 1U as as 8-bit type variables. So use 1UL and typecast the value
+ * to 'unsigned int'
+ */
+#define SMC_SVE_HINT		((unsigned int)(1UL << 16))
 
 /* FID: Function number */
 #define SMC_FNUM_SHIFT		U(0)
