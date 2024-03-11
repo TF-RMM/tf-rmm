TF-RMM/tf-rmm

commit 862f96ce3beacc8dd85b88e80e7dfe9ba93d5c87

feat(lib/realm): use DZ ZVA instruction for granule zeroing
This patch modifies granule_memzero_mapped() function
by replacing calls to memset() with DC ZVA instructions
executed in a loop.

Change-Id: I8dc012d337032ddcd78e22323dcc07089113e5ee
Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

lib/arch/CMakeLists.txt
@@ -37,5 +37,6 @@ if(NOT RMM_ARCH STREQUAL fake_host)
 else()
     target_sources(rmm-lib-arch
         PRIVATE "src/fake_host/cache_wrappers.c"
+                "src/fake_host/instr_helpers.c"
                 "src/fake_host/simd_helpers.c")
 endif()

plat/host/common/src/host_utils.c
@@ -208,6 +208,13 @@ void host_util_setup_sysreg_and_boot_manifest(void)
 	(void)host_util_set_default_sysreg_cb("pmcr_el0",
 			INPLACE(PMCR_EL0_N, 31UL));
 
+	/*
+	 * Set DCZID_EL0 register with DZP = 0 and
+	 * BS = 0b1001 with the maximum supported block size 2KB.
+	 */
+	(void)host_util_set_default_sysreg_cb("dczid_el0",
+			INPLACE(DCZID_EL0_BS, 9UL));
+
 	/* Set ISR_EL1 to 0 */
 	ret = host_util_set_default_sysreg_cb("isr_el1", 0UL);
 

lib/arch/include/aarch64/instr_helpers.h
@@ -74,4 +74,12 @@ static inline void (_op ## _type)(uint64_t v)		\
 #define dsb(scope) asm volatile("dsb " #scope : : : "memory")
 #define dmb(scope) asm volatile("dmb " #scope : : : "memory")
 
+/*
+ * These additional defines allow Arch-specific implementations different
+ * from the common template.
+ */
+
+/* DC ZVA, Data Cache Zero by VA instruction */
+#define DEFINE_SYSOP_DCZVA	DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, zva)
+
 #endif /* INSTR_HELPERS_H */

lib/arch/include/arch.h
@@ -154,6 +154,7 @@
 #define HCR_TLOR	(UL(1) << 35)
 #define HCR_E2H		(UL(1) << 34)
 #define HCR_RW		(UL(1) << 31)
+#define HCR_TDZ		(UL(1) << 28)
 #define HCR_TGE		(UL(1) << 27)
 #define HCR_TSW		(UL(1) << 22)
 #define HCR_TACR	(UL(1) << 21)
@@ -706,6 +707,11 @@
 #define PMCR_EL0_INIT			(PMCR_EL0_LC_BIT | PMCR_EL0_DP_BIT)
 #define PMCR_EL0_INIT_RESET		(PMCR_EL0_INIT | PMCR_EL0_C_BIT | \
 					 PMCR_EL0_P_BIT)
+
+/* DCZID_EL0 Definitions */
+#define DCZID_EL0_BS_SHIFT		0
+#define DCZID_EL0_BS_WIDTH		U(4)
+#define DCZID_EL0_DZP_BIT		(UL(1) << 4)
 
 /* MDSCR_EL1 Definitions */
 #define MDSCR_EL1_TDCC_BIT	(UL(1) << 12)

lib/arch/include/arch_helpers.h
@@ -62,7 +62,7 @@ DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, cvac)
 DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, ivac)
 DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, civac)
 DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, cvau)
-DEFINE_SYSOP_TYPE_PARAM_FUNC(dc, zva)
+DEFINE_SYSOP_DCZVA
 
 /*******************************************************************************
  * Address translation accessor prototypes
@@ -418,6 +418,8 @@ DEFINE_SYSREG_RW_FUNCS(pmscr_el2)
 
 DEFINE_RENAME_SYSREG_RW_FUNCS(fpcr, FPCR)
 DEFINE_RENAME_SYSREG_RW_FUNCS(fpsr, FPSR)
+
+DEFINE_SYSREG_READ_FUNC(dczid_el0)
 
 /*******************************************************************************
  * Timer register accessor prototypes

lib/arch/include/fake_host/instr_helpers.h
@@ -82,4 +82,13 @@ static inline void (_op ## _type)(uint64_t v)		\
 #define dsb(scope)
 #define dmb(scope)
 
+/*
+ * These additional defines allow Arch-specific implementations different
+ * from the common template.
+ */
+
+/* DC ZVA, Data Cache Zero by VA instruction */
+#define DEFINE_SYSOP_DCZVA
+void dczva(uint64_t addr);
+
 #endif /* INSTR_HELPERS_H */

lib/arch/src/fake_host/instr_helpers.c
@@ -1,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#include <arch_helpers.h>
+#include <string.h>
+
+/*******************************************************************************
+ * Mock DC ZVA, Data Cache Zero by VA instruction
+ ******************************************************************************/
+void dczva(uint64_t addr)
+{
+	(void)memset((void *)addr, 0,
+		1U << (EXTRACT(DCZID_EL0_BS, read_dczid_el0()) + 2U));
+}

lib/realm/src/granule.c
@@ -3,6 +3,7 @@
  * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
  */
 
+#include <arch_helpers.h>
 #include <assert.h>
 #include <buffer.h>
 #include <debug.h>
@@ -247,13 +248,35 @@ void granule_memzero(struct granule *g, enum buffer_slot slot)
 	buf = granule_map(g, slot);
 	assert(buf != NULL);
 
-	(void)memset(buf, 0, GRANULE_SIZE);
+	granule_memzero_mapped(buf);
 	buffer_unmap(buf);
 }
 
 void granule_memzero_mapped(void *buf)
 {
-	(void)memset(buf, 0, GRANULE_SIZE);
+	unsigned long dczid_el0 = read_dczid_el0();
+	uintptr_t addr = (uintptr_t)buf;
+	unsigned int log2_size;
+	unsigned int block_size;
+	unsigned int cnt;
+
+	/* Check that use of DC ZVA instructions is permitted */
+	assert((dczid_el0 & DCZID_EL0_DZP_BIT) == 0UL);
+
+	/*
+	 * Log2 of the block size in words.
+	 * The maximum size supported is 2KB, indicated by value 0b1001.
+	 */
+	log2_size = (unsigned int)EXTRACT(DCZID_EL0_BS, dczid_el0) + 2U;
+	block_size = U(1) << log2_size;
+
+	/* Number of iterations */
+	cnt = U(1) << (GRANULE_SHIFT - log2_size);
+
+	for (unsigned int i = 0U; i < cnt; i++) {
+		dczva(addr);
+		addr += block_size;
+	}
 }
 
 /*

commit 309e537dc2667c94aaf8b3b541d7e7a36061f137

fix(plat/host): Fix CBMC errors related to DCZVA patch
The patch 862f96c introduced support for DCZVA but it also introduced
some errors for CBMC analysis. Since the GRANULE_SHIFT is 7 for CBMC
builds, the value of 9 for DCZID_EL0_BS in host platform caused overflow
for one of the calculations. This patch reduces the value for host
platform to 5.

The `cbmc-gcc` build target is also fixed by this patch.

Signed-off-by: Soby Mathew <soby.mathew@arm.com>
Change-Id: I210d54723f36b1aae07caffc3bfa986cf2341e55

lib/arch/CMakeLists.txt
@@ -18,7 +18,8 @@ target_include_directories(rmm-lib-arch
 if(HOST_VARIANT STREQUAL "host_cbmc")
   target_sources(rmm-lib-arch
         PRIVATE "src/fake_host/cbmc_arch_features.c"
-                "src/fake_host/cbmc_simd.c")
+                "src/fake_host/cbmc_simd.c"
+                "src/fake_host/instr_helpers.c")
     # In case of 'host_cbmc' variant no further source files are added to
     # the build.
     return()

plat/host/common/src/host_utils.c
@@ -210,10 +210,10 @@ void host_util_setup_sysreg_and_boot_manifest(void)
 
 	/*
 	 * Set DCZID_EL0 register with DZP = 0 and
-	 * BS = 0b1001 with the maximum supported block size 2KB.
+	 * BS = 0b101 as GRANULE_SIZE on CBMC platform is 7.
 	 */
 	(void)host_util_set_default_sysreg_cb("dczid_el0",
-			INPLACE(DCZID_EL0_BS, 9UL));
+			INPLACE(DCZID_EL0_BS, 5UL));
 
 	/* Set ISR_EL1 to 0 */
 	ret = host_util_set_default_sysreg_cb("isr_el1", 0UL);