cf1c2f6a3dd36c2167d940dc7164423a404fb53b
Andre Przywara <andre.przywara@arm.com>
2024-03-19 14:13:38 +0000

    fix(toolchains/aarch64): replace ZIP_LISTS with string REPLACE
    When trying to search for the highest architecture revision supported by
    the compiler, we need to use a string in the form of "8.5" for the
    compiler option, but something like "8_5" for the internal variables.
    At the moment this is solved with the help of cmake's ZIP_LISTS feature.
    However this was only introduced in cmake 3.17, but our recommended
    build platform (Ubuntu 20.04) ships with cmake 3.16.3 only.
    
    Replace the usage of ZIP_LISTS with a simple "string(REPLACE" command,
    that is compatible with much older cmake versions.
    
    This allows to build RMM with Ubuntu's stock cmake again.
    
    Change-Id: I24df30228ed3100f1508af84dfc8e6b577693f64
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- toolchains/aarch64/common_aarch64.cmake
+++ toolchains/aarch64/common_aarch64.cmake
@@ -28,9 +28,9 @@ string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-pie ")
 # Detect applicable "march=" option supported by compiler
 function(detect_and_set_march)
 	set (march_list 9.2 9.1 9 8.8 8.7 8.6 8.5)
-	set (march_num 9_2 9_1 9 8_8 8_7 8_6 8_5)
 
-	foreach(v n IN ZIP_LISTS march_list march_num)
+	foreach(v ${march_list})
+		string(REPLACE "." "_" n ${v})
 		check_c_compiler_flag("-march=armv${v}-a" COMPILER_SUPPORTS_${n})
 		if(COMPILER_SUPPORTS_${n})
 			set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv${v}-a" PARENT_SCOPE)
