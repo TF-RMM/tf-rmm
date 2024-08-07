9a704090252d6a29e30359fa0b4faf62e9b81608
Mate Toth-Pal <mate.toth-pal@arm.com>
2024-08-07 15:01:41 +0200

    feat(app/common): initialise console for apps
    Change-Id: I0cc0988ae477b24817409a4f3a6bb6e815cde28b
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- app/common/el0_app/src/el0_app_helpers.c
+++ app/common/el0_app/src/el0_app_helpers.c
@@ -3,9 +3,50 @@
  * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
  */
 
+#include <app_common.h>
+#include <console.h>
 #include <debug.h>
+#include <el0_app_helpers.h>
+#include <errno.h>
+#include <limits.h>
 
+int app_console_putc(int character, const struct console *console);
+static void app_console_flush(const struct console *console);
+void init_console(void);
 void mbedtls_exit_panic(unsigned int reason);
+
+static struct console app_console = {
+	.flush = app_console_flush,
+	.putc = app_console_putc
+};
+
+int app_console_putc(int character, const struct console *console)
+{
+	signed char *shared_buf = (signed char *)get_shared_mem_start();
+	unsigned long ret;
+
+	(void) console;
+	shared_buf[0] = (signed char)character;
+	ret = el0_app_service_call(APP_SERVICE_PRINT, 1, 0, 0, 0);
+
+	if (ret > (unsigned long)INT_MAX) {
+		return -1;
+	} else {
+		return (int)ret;
+	}
+
+}
+
+static void app_console_flush(const struct console *console)
+{
+	(void)console;
+	/* Do nothing */
+}
+
+void init_console(void)
+{
+	(void)console_register(&app_console);
+}
 
 /* Used by Mbed TLS buffer alloc */
 void mbedtls_exit_panic(unsigned int reason)
