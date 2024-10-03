cd44002fc37ec86870016dc5caa193be9fb7a9bb
Soby Mathew <soby.mathew@arm.com>
2024-10-03 12:07:00 +0100

    feat(lib/rmm_el3_ifc): support for attestation via EL3
    Add support functions to be able to push attestation signing
    requests to EL3. The patch also adds some additional comments
    to the rmm_el3_ifc.h file
    
    Change-Id: I801ac4ab9cb232e8e8a5a54ec1238048f4285f51
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

--- lib/rmm_el3_ifc/include/rmm_el3_ifc.h
+++ lib/rmm_el3_ifc/include/rmm_el3_ifc.h
@@ -10,12 +10,13 @@
 #include <smc.h>
 
 #ifndef __ASSEMBLER__
+#include <stdbool.h>
 #include <stddef.h>
 #endif
 
-/*************************************
- * SMC codes for the EL3-RMM interface
- *************************************/
+/***************************************
+ * Error codes for the EL3-RMM interface
+ ***************************************/
 
 #define E_RMM_OK			 (0)
 #define E_RMM_UNK			(-1)
@@ -24,6 +25,20 @@
 #define E_RMM_NOMEM			(-4)
 #define E_RMM_INVAL                     (-5)
 #define E_RMM_AGAIN			(-6)
+
+/****************************************
+ * Generic defines for RMM-EL3 interface
+ ****************************************/
+
+/* Signature Algorithm ID */
+#define ATTEST_KEY_CURVE_ECC_SECP384R1  (0U)
+
+/* Hash Algorithm ID */
+#define EL3_TOKEN_SIGN_HASH_ALG_SHA384	(1U)
+
+/********************************************
+ * SMC Function IDs for the EL3-RMM interface
+ ********************************************/
 
 					/* 0x1B0 - 0x1B1 */
 #define SMC_RMM_GTSI_DELEGATE		SMC64_STD_FID(RMM_EL3, U(0))
@@ -46,12 +61,20 @@
 #define E_RMM_BOOT_MANIFEST_VERSION_NOT_SUPPORTED	(-6)
 #define E_RMM_BOOT_MANIFEST_DATA_ERROR			(-7)
 
+/* Starting RMM-EL3 interface version 0.4 */
+					/* 0x1B4 */
+#define SMC_RMM_EL3_FEATURES		SMC64_STD_FID(RMM_EL3, U(4))
+
+					/* 0x1B5 */
+#define SMC_RMM_EL3_TOKEN_SIGN		SMC64_STD_FID(RMM_EL3, U(5))
+
+
 /************************
  * Version related macros
  ************************/
 
 /*
- * Boot Interface and Manifest versions encoding:
+ * RMM-EL3 Interface and Boot Manifest versions encoding:
  *	- Bit[31] RES0
  *	- Bits [30:16] Major version
  *	- Bits [15:0] Minor version
@@ -65,13 +88,13 @@
 	(((((_major) & 0x7FFFU) << 16) | ((_minor) & 0xFFFFU)))
 
 /*
- * The Major version value for the Boot Interface supported by this
+ * The Major version value for the RMM-EL3 Interface supported by this
  * implementation of RMM.
  */
 #define RMM_EL3_IFC_VERS_MAJOR		(U(0))
 
 /*
- * The Minor version value for the Boot interface supported by this
+ * The Minor version value for the RMM-EL3 Interface supported by this
  * implementation of RMM.
  */
 #define RMM_EL3_IFC_VERS_MINOR		(U(3))
@@ -113,12 +136,61 @@
 	 (RMM_EL3_MANIFEST_GET_VERS_MINOR(_version) >= RMM_EL3_MANIFEST_VERS_MINOR))
 
 #ifndef __ASSEMBLER__
-/****************************************************************************
- * Boot interface related functions
+
+/***************************************************************************
+ * SMC_RMM_EL3_FEATURES related definitions
+ ***************************************************************************/
+#define RMM_EL3_IFC_FEAT_REG_0_IDX			U(0)
+#define RMM_EL3_IFC_FEAT_REG_0_EL3_TOKEN_SIGN_SHIFT	U(0)
+#define RMM_EL3_IFC_FEAT_REG_0_EL3_TOKEN_SIGN_WIDTH	U(1)
+
+/***************************************************************************
+ * SMC_RMM_EL3_TOKEN_SIGN related definitions and structures
+ ***************************************************************************/
+
+/* Command code for SMC_RMM_EL3_TOKEN_SIGN SMC */
+#define SMC_RMM_EL3_TOKEN_SIGN_PUSH_REQ_OP		U(1)
+#define SMC_RMM_EL3_TOKEN_SIGN_PULL_RESP_OP		U(2)
+#define SMC_RMM_EL3_TOKEN_SIGN_GET_RAK_PUB_OP		U(3)
+
+/* The Max hash size is set to be SHA-512 Digest size */
+#define EL3_TOKEN_REQUEST_MAX_HASH_LEN			64
+/* The Max Signature is set to be a buffer of 512 Bytes (4096 bits) */
+#define EL3_TOKEN_RESPONSE_MAX_SIG_LEN			512
+
+/* Structure format in which EL3 expects a request */
+struct el3_token_sign_request {
+	SET_MEMBER(uint32_t sign_alg_id, 0x0, 0x8);
+	SET_MEMBER(uint64_t cookie, 0x8, 0x10);		/* Passthrough to response */
+	SET_MEMBER(uint64_t req_ticket, 0x10, 0x18);	/* Passthrough to response */
+	SET_MEMBER(uint32_t hash_alg_id, 0x18, 0x20);
+	SET_MEMBER(uint8_t hash_buf[EL3_TOKEN_REQUEST_MAX_HASH_LEN], 0x20, 0x60);
+};
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_request, sign_alg_id)) == 0x0U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_request, cookie)) == 0x8U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_request, req_ticket)) == 0x10U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_request, hash_alg_id)) == 0x18U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_request, hash_buf)) == 0x20U);
+
+/* Structure format in which EL3 is expected to return data */
+struct el3_token_sign_response {
+	SET_MEMBER(uint64_t cookie, 0x0, 0x8);		/* Passthrough from request */
+	SET_MEMBER(uint64_t req_ticket, 0x8, 0x10);	/* Passthrough from request */
+	SET_MEMBER(uint16_t sig_len, 0x10, 0x12);
+	SET_MEMBER(uint8_t signature_buf[EL3_TOKEN_RESPONSE_MAX_SIG_LEN], 0x12, 0x212);
+};
+
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_response, cookie)) == 0x0U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_response, req_ticket)) == 0x8U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_response, sig_len)) == 0x10U);
+COMPILER_ASSERT(U(offsetof(struct el3_token_sign_response, signature_buf)) == 0x12U);
+
+/***************************************************************************
+ * RMM-EL3 Interface related functions
  ***************************************************************************/
 
 /*
- * Accessors to the parameters obtained through the boot interface arguments.
+ * Accessors to the parameters obtained through the RMM-EL3 Interface arguments.
  */
 unsigned int rmm_el3_ifc_get_version(void);
 uintptr_t rmm_el3_ifc_get_shared_buf_pa(void);
@@ -129,8 +201,9 @@ static inline size_t rmm_el3_ifc_get_shared_buf_size(void)
 }
 
 /*
- * Validate the boot arguments and Initialize the rmm_el3_ifc library.
- * This function must be called only once during cold boot.
+ * Validate the RMM-EL3 Interface boot arguments and initialize the
+ * rmm_el3_ifc library. This function must be called only once during cold
+ * boot.
  *
  * This function must be called prior to enable the MMU and data cache for
  * RMM execution.
@@ -277,7 +350,7 @@ int rmm_el3_ifc_get_dram_data_validated_pa(unsigned long max_num_banks,
 int rmm_el3_ifc_get_console_list_pa(struct console_list **plat_console_list);
 
 /****************************************************************************
- * RMM-EL3 Runtime APIs
+ * RMM-EL3 Runtime interface APIs
  ***************************************************************************/
 
 /*
@@ -294,7 +367,11 @@ int rmm_el3_ifc_get_console_list_pa(struct console_list **plat_console_list);
  *	- crv:		ECC Crve type for querying attestation key from monitor.
  *
  * Return:
- *	- 0 On success or a negative error code otherwise.
+ *	- E_RMM_OK	On Success.
+ *	- E_RMM_INVAL	If the arguments are invalid.
+ *	- E_RMM_AGAIN	EL3 is busy and the key cannot be retrieved at this
+ *			time. At least one further call will be needed in
+ *			order to retrieve the key.
  */
 int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
 					size_t *len, unsigned int crv);
@@ -362,6 +439,86 @@ static inline unsigned long rmm_el3_ifc_gtsi_undelegate(unsigned long addr)
  *	- ec:		SMC_RMM_BOOT_COMPLETE return code
  */
 __dead2 void rmm_el3_ifc_report_fail_to_el3(int ec);
+
+/*
+ * Query if EL3_TOKEN_SIGN feature is supported by EL3 firmware.
+ * Return true or false depending on the support.
+ */
+bool rmm_el3_ifc_el3_token_sign_supported(void);
+
+/*
+ * Push the attestation signing request to EL3 firmware.
+ * Optional interface from the RMM-EL3 interface v0.4 onwards.
+ *
+ * Args:
+ *	- req:		Pointer to the token sign request to be pushed to EL3.
+ *			The structure must be located in the RMM-EL3 shared
+ *			memory buffer and must be locked before use.
+ *
+ * Return:
+ *	- E_RMM_OK	On Success.
+ *	- E_RMM_INVAL	If the arguments are invalid.
+ *	- E_RMM_AGAIN	Indicates that the request was not queued since the
+ *			queue in EL3 is full.
+ *	- E_RMM_UNK	If the SMC is not implemented or if interface
+ *			version is < 0.4.
+ */
+int rmm_el3_ifc_push_el3_token_sign_request(const struct el3_token_sign_request *req);
+
+/*
+ * Pull the attestation signing response from the EL3 firmware.
+ * Optional interface from the RMM-EL3 interface v0.4 onwards.
+ * Args:
+ *	- resp:		Pointer to the token sign response to get from EL3.
+ *			The structure must be located in the RMM-EL3 shared
+ *			memory buffer and must be locked before use.
+ *
+ * Return:
+ *	- E_RMM_OK	On Success.
+ *	- E_RMM_INVAL	If the arguments are invalid.
+ *	- E_RMM_AGAIN	Indicates that a response is not ready yet.
+ *	- E_RMM_UNK	If the SMC is not implemented or if interface
+ *			version is < 0.4.
+ */
+int rmm_el3_ifc_pull_el3_token_sign_response(const struct el3_token_sign_response *resp);
+
+/*
+ * Get the realm attestation public key when EL3 is used to sign attestaion
+ * tokens.
+ * Optional interface from the RMM-EL3 interface v0.4 onwards.
+ * Args:
+ *	- buf		Pointer to the buffer used to get the attestation public key
+ *			This must belong to the RMM-EL3 shared memory and must be locked
+ *			before use.
+ *	- buflen	The size of the buffer `buf`.
+ *	- len		Pointer to a size_t variable to store the size of the
+ *			received response.
+ *	- crv		The ECC curve for which the public key is requested.
+ *
+ * Return:
+ *	- E_RMM_OK	On Success.
+ *	- E_RMM_INVAL	If the arguments are invalid.
+ *	- E_RMM_AGAIN	Indicates that a response is not ready yet.
+ *	- E_RMM_UNK	If the SMC is not implemented or if interface
+ *			version is < 0.4.
+ */
+int rmm_el3_ifc_get_realm_attest_pub_key_from_el3(uintptr_t buf, size_t buflen,
+					size_t *len, unsigned int crv);
+
+/*
+ * Access the feature register. This is supported for interface version 0.4 and
+ * later.
+ *
+ * Args:
+ * 	- feat_reg_idx	The feature register index.
+ *	- feat_reg	Pointer to store the value of the register.
+ * Return:
+ *	- E_RMM_OK	On Success.
+ *	- E_RMM_INVAL	If the arguments are invalid.
+ *	- E_RMM_UNK	If the SMC is not present if interface
+ *			version is < 0.4.
+ */
+int rmm_el3_ifc_get_feat_register(unsigned int feat_reg_idx, uint64_t *feat_reg);
 
 #endif /* __ASSEMBLER__ */
 #endif /* RMM_EL3_IFC_H */
