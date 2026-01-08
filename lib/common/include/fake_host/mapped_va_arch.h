6ea21a7e67507f485c5b6dffb30a63f04bdf79d6
Soby Mathew <soby.mathew@arm.com>
2026-01-08 16:41:16 +0000

    feat(lib/common): Add MAPPED_VA_ARCH() macro
    This macro is used to abstract difference between AArch64 target and
    fake_host. On the target, the mapped VA should be used to access memory
    where PA is used on fake_host.
    
    Change-Id: Id99189e4d4a2c0c19fdebf24918bdec219d991b6
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>

---
+++ lib/common/include/fake_host/mapped_va_arch.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef MAPPED_VA_ARCH_H
+#define MAPPED_VA_ARCH_H
+
+/*
+ * On AArch64, virtual addresses are used directly for memory access.
+ * On fake_host, physical addresses are used instead.
+ */
+#define MAPPED_VA_ARCH(_va, _pa)	(_pa)
+
+#endif /* MAPPED_VA_ARCH_H */
